7729d98162c48a1efa113f45656fdb370c583180
Illyoung Choi <iychoi@arizona.edu>
2026-09-02 13:13:43 -0700

    handle short read/write

--- irods/common/irods_io.go
+++ irods/common/irods_io.go
@@ -21,33 +21,38 @@ func ReadDataObject(filesystem *irodsclient_fs.FileSystem, sourcePath string, of
 	}
 	defer handle.Close()
 
-	// read the file content
 	buffer := make([]byte, maxReadLen)
-
-	n, err := handle.ReadAt(buffer, offset)
-	if err != nil {
+	total := 0
+	for total < len(buffer) {
+		n, err := handle.ReadAt(buffer[total:], offset+int64(total))
+		total += n
 		if err == io.EOF {
-			// EOF is not an error
-			return buffer[:n], nil
+			break
 		}
-
-		return nil, errors.Wrapf(err, "failed to read file %q at offset %d length %d", sourcePath, offset, maxReadLen)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to read file %q at offset %d length %d", sourcePath, offset, maxReadLen)
+		}
 	}
 
-	return buffer[:n], nil
+	return buffer[:total], nil
 }
 
 func WriteDataObject(filesystem *irodsclient_fs.FileSystem, destPath string, offset int64, content []byte) error {
+	// "w" = FileOpenModeWriteOnly: write without truncation.
+	// Use "w+" (FileOpenModeWriteTruncate) only when a full overwrite is intended.
 	handle, err := filesystem.OpenFile(destPath, "", "w")
 	if err != nil {
 		return errors.Wrapf(err, "failed to open file %q", destPath)
 	}
 	defer handle.Close()
 
-	// write the file content
-	_, err = handle.WriteAt(content, offset)
-	if err != nil {
-		return errors.Wrapf(err, "failed to write file %q at offset %d length %d", destPath, offset, len(content))
+	written := 0
+	for written < len(content) {
+		n, err := handle.WriteAt(content[written:], offset+int64(written))
+		written += n
+		if err != nil {
+			return errors.Wrapf(err, "failed to write file %q at offset %d length %d", destPath, offset, len(content))
+		}
 	}
 
 	return nil
