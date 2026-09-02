8cf1ab9cf8e68e542803dbf7bd63e01f40b19c54
Illyoung Choi <iychoi@arizona.edu>
2026-09-02 14:27:38 -0700

    init system attributes map

--- irods/common/irods_metadata.go
+++ irods/common/irods_metadata.go
@@ -1,24 +1,20 @@
 package common
 
 var (
-	systemAttributes   []string        = []string{"ipc_UUID"}
-	systemAttributeMap map[string]bool = map[string]bool{}
+	systemAttributes   = []string{"ipc_UUID"}
+	systemAttributeMap = map[string]bool{}
 )
+
+func init() {
+	for _, a := range systemAttributes {
+		systemAttributeMap[a] = true
+	}
+}
 
 func GetSystemAttributes() []string {
 	return systemAttributes
 }
 
 func IsSystemAttribute(attr string) bool {
-	if len(systemAttributeMap) != len(systemAttributes) {
-		for _, a := range systemAttributes {
-			systemAttributeMap[a] = true
-		}
-	}
-
-	if _, ok := systemAttributeMap[attr]; ok {
-		// has it
-		return true
-	}
-	return false
+	return systemAttributeMap[attr]
 }
